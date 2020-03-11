f5675f8fd8f536090c763f8c71e235f9f5e9e63b
adrian alberto nunez capote <aanunez@estudiantes.uci.cu>
2020-03-11 02:59:28 -0400

    Plannig's models

--- api/planning/admin.py
+++ api/planning/admin.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from planning.models import Iteration
+from planning.models import Personal
+
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Iteration)
+admin.site.register(Personal)
